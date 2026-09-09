799f16dad197da5172be9aa7a92f9827fe61b53a
Thomas Picart <thomas.picart90@gmail.com>
2026-09-09 11:31:14 +0200

    feat: add base Entity class for all game objects
    - add Core/Entity.h defining abstract base entity
    - provide sprite, alive flag, update/collision/draw interface

---
+++ Game/src/Core/Entity.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+#include <optional>
+
+class Entity
+{
+protected:
+    std::optional<sf::Sprite> sprite;
+    bool alive = true;
+
+public:
+    virtual ~Entity() = default;
+
+    virtual void Update(float deltaTime) = 0;
+    virtual void OnCollision(Entity* other) = 0;
+
+    virtual void Render(sf::RenderWindow& window)
+    {
+        if (sprite.has_value())
+            window.draw(*sprite);
+    }
+
+    sf::FloatRect getBounds() const
+    {
+        return sprite.has_value() ? sprite->getGlobalBounds() : sf::FloatRect();
+    }
+    
+    bool isAlive() const
+    {
+        return alive;
+    }
+    
+};
